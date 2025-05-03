a06964076c8d6f5abd2aa67d49b8f1ac26a10fa2
harsh170699 <harsh.shah11@einfochips.com>
2025-05-03 12:12:26 +0530

    Updated Total budget

--- src/app/budget-item-list/budget-item-list.component.ts
+++ src/app/budget-item-list/budget-item-list.component.ts
@@ -2,7 +2,6 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { BudgetItem } from 'src/shared/models/budget-item.model';
 import { EditItemModalComponent } from '../edit-item-modal/edit-item-modal.component';
-
 @Component({
   selector: 'app-budget-item-list',
   templateUrl: './budget-item-list.component.html',
@@ -12,6 +11,7 @@ export class BudgetItemListComponent implements OnInit {
 
   @Input() budgetItems: BudgetItem[] = [];
   @Output() delete: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+  @Output() update: EventEmitter<any> = new EventEmitter<any>();
 
   constructor(public dialog: MatDialog) { }
 
@@ -31,8 +31,18 @@ export class BudgetItemListComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
         // replace the item with the updated/submitted item from the form
-        this.budgetItems[this.budgetItems.indexOf(item)] = result;
+        // this.budgetItems[this.budgetItems.indexOf(item)] = result;
+        this.update.emit({
+          old: item,
+          new: result
+        })
       }
     });
   }
+}
+
+
+export interface UpdateEvent {
+  old: BudgetItem;
+  new: BudgetItem;
 }
